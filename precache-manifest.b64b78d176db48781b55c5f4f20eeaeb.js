1f31a3c14327b094193da4c8f97de3226005bbb8
Tanuja Bameta <tanuja@fvfz98xdj1wk.bbrouter>
2020-09-18 18:59:00 +0530

    Updates

---
+++ precache-manifest.b64b78d176db48781b55c5f4f20eeaeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1111a6b241b48d12c8542640a110262f",
+    "url": "/autoComplete/index.html"
+  },
+  {
+    "revision": "5ccb2c39c7ec8d14d037",
+    "url": "/autoComplete/static/css/main.613da5f4.chunk.css"
+  },
+  {
+    "revision": "a3f0b54ab8573435c42d",
+    "url": "/autoComplete/static/js/2.3ef743cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/autoComplete/static/js/2.3ef743cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ccb2c39c7ec8d14d037",
+    "url": "/autoComplete/static/js/main.139c968c.chunk.js"
+  },
+  {
+    "revision": "2bf50292efd44a0fb60e",
+    "url": "/autoComplete/static/js/runtime-main.2bd6affb.js"
+  }
+]);
